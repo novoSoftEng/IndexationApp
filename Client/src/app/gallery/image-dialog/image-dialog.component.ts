27bbb0f334bfdec169fbf0c7375a731868f2d607
secr <ch.liefrid@gmail.com>
2024-12-05 10:39:44 +0100

    added delete and download button

--- Client/src/app/gallery/image-dialog/image-dialog.component.ts
+++ Client/src/app/gallery/image-dialog/image-dialog.component.ts
@@ -7,6 +7,7 @@ import { MatDialogModule ,MAT_DIALOG_DATA,
   MatDialogRef,
   MatDialogTitle } from '@angular/material/dialog';
 import { GalleryComponent } from '../gallery.component';
+import { ImageService } from '../../Services/image.service';
 @Component({
   selector: 'app-image-dialog',
   standalone: true,
@@ -15,11 +16,30 @@ import { GalleryComponent } from '../gallery.component';
   styleUrl: './image-dialog.component.css'
 })
 export class ImageDialogComponent {
+  constructor (private imageService: ImageService){}
   readonly dialogRef = inject(MatDialogRef<GalleryComponent>);
   readonly data = inject<any>(MAT_DIALOG_DATA);
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+  // Download the image
+  downloadImage(): void {
+    const link = document.createElement('a');
+    link.href = this.data.url; // URL of the image
+    link.download = this.data.image; // Suggested file name
+    link.click();
+  }
+
+  // Trigger delete action
+  deleteImage(): void {
+    // Emit or handle the deletion process
+    console.log('Delete action triggered for:', this.data.image);
+    this.imageService.deleteFile(this.data.image).subscribe((msg)=>{
+      console.log("image deleted",msg.message);
+    })
+
+    this.dialogRef.close({ action: 'delete', image: this.data.image });
   }
 
 }
